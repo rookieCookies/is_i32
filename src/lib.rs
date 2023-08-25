fe18889af5143dcebff526b15fc14aecdac79fae
*daymare* <87831835+rookieCookies@users.noreply.github.com>
2023-08-26 01:39:28 +0300

    performacne fix

---
+++ src/lib.rs
@@ -0,0 +1,272 @@
+#![allow(clippy::all)]
+#![allow(unused)]
+#![allow(non_snake_case)]
+#![allow(confusable_idents)]
+#![allow(uncommon_codepoints)]
+
+use std::{thread, time::{self, Duration}};
+
+struct TimeDuration(time::Duration);
+
+trait WhatDidYouThinkUdSayToThat {
+    fn does_a_scorpion_sting_when_fighting_back(self, other: Self) -> Self;
+}
+
+trait TheyStrikeToKillAndYouKnowIWill {
+    fn you_know_i_will(self, other: Self) -> Self;
+}
+
+trait WhatDoYouSingOnYourDriveHome {
+    fn does_she_smile(&self, other: &Self) -> bool;
+}
+
+trait OrDoesSheMouthFuckYouForever {
+    fn every_time_you_call_me_crazy_i_get_more_crazy(x: i32) -> Self;
+}
+
+trait WhatAboutThat: Copy {}
+
+trait AndWhenYouSayISeemAngryIGetMoreAngry {
+    fn and_theres_nothing_like_a_mad_woman(&self, x: Self) -> Self;
+}
+
+trait WhatAShameSheWentMad {
+    fn no_one_likes_a_mad_woman(&self, x: Self) -> Self;
+}
+
+trait YouMadeHerLikeThat {
+    fn and_youll_poke_that_bear_till_her_claws_come_out(&self) -> Self;
+}
+
+struct AndYoullFindSomethingToWrapYourNooseAround(TimeDuration);
+
+impl AndYoullFindSomethingToWrapYourNooseAround {
+    fn and_thers_thing_like_a_mad_woman(&self) {
+        #[cfg(target_os = "windows")]
+        thread::sleep(Duration::from_millis(10));
+    }
+}
+
+#[derive(Clone, Copy)]
+struct NowIBreatheFlamesEachTimeITalk;
+
+impl WhatDidYouThinkUdSayToThat for NowIBreatheFlamesEachTimeITalk {
+    fn does_a_scorpion_sting_when_fighting_back(self, other: Self) -> Self {
+        NowIBreatheFlamesEachTimeITalk
+    }
+}
+
+impl TheyStrikeToKillAndYouKnowIWill for NowIBreatheFlamesEachTimeITalk {
+    fn you_know_i_will(self, other: Self) -> Self {
+        NowIBreatheFlamesEachTimeITalk
+    }
+}
+
+impl WhatDoYouSingOnYourDriveHome for NowIBreatheFlamesEachTimeITalk {
+    fn does_she_smile(&self, _other: &Self) -> bool {
+        true
+    }
+}
+
+impl OrDoesSheMouthFuckYouForever for NowIBreatheFlamesEachTimeITalk {
+    fn every_time_you_call_me_crazy_i_get_more_crazy(_x: i32) -> Self {
+        NowIBreatheFlamesEachTimeITalk
+    }
+}
+
+impl WhatAboutThat for NowIBreatheFlamesEachTimeITalk {}
+
+impl AndWhenYouSayISeemAngryIGetMoreAngry for NowIBreatheFlamesEachTimeITalk {
+    fn and_theres_nothing_like_a_mad_woman(&self, _x: Self) -> Self {
+        NowIBreatheFlamesEachTimeITalk
+    }
+}
+
+impl WhatAShameSheWentMad for NowIBreatheFlamesEachTimeITalk {
+    fn no_one_likes_a_mad_woman(&self, _x: Self) -> Self {
+        NowIBreatheFlamesEachTimeITalk
+    }
+}
+
+impl YouMadeHerLikeThat for NowIBreatheFlamesEachTimeITalk {
+    fn and_youll_poke_that_bear_till_her_claws_come_out(&self) -> Self {
+        NowIBreatheFlamesEachTimeITalk
+    }
+}
+
+impl WhatDidYouThinkUdSayToThat for i32 {
+    fn does_a_scorpion_sting_when_fighting_back(self, other: Self) -> Self {
+        self + other
+    }
+}
+
+impl TheyStrikeToKillAndYouKnowIWill for i32 {
+    fn you_know_i_will(self, other: Self) -> Self {
+        self.saturating_mul(other)
+    }
+}
+
+impl WhatDoYouSingOnYourDriveHome for i32 {
+    fn does_she_smile(&self, other: &Self) -> bool {
+        *self == *other
+    }
+}
+
+impl OrDoesSheMouthFuckYouForever for i32 {
+    fn every_time_you_call_me_crazy_i_get_more_crazy(x: i32) -> Self {
+        x
+    }
+}
+
+impl WhatAboutThat for i32 {}
+
+impl AndWhenYouSayISeemAngryIGetMoreAngry for i32 {
+    fn and_theres_nothing_like_a_mad_woman(&self, x: Self) -> Self {
+        self + x
+    }
+}
+
+impl WhatAShameSheWentMad for i32 {
+    fn no_one_likes_a_mad_woman(&self, x: Self) -> Self {
+        self.wrapping_mul(x)
+    }
+}
+
+impl YouMadeHerLikeThat for i32 {
+    fn and_youll_poke_that_bear_till_her_claws_come_out(&self) -> Self {
+        self.abs()
+    }
+}
+
+fn my_cannons_all_firin_at_your_yacht<T>(x: T) -> T
+where
+    T: WhatDidYouThinkUdSayToThat + Clone,
+{
+    x.clone().does_a_scorpion_sting_when_fighting_back(x)
+}
+
+fn they_say_move_on_but_you_know_i_wont<T>(x: T) -> T
+where
+    T: TheyStrikeToKillAndYouKnowIWill + OrDoesSheMouthFuckYouForever,
+{
+    x.you_know_i_will(OrDoesSheMouthFuckYouForever::every_time_you_call_me_crazy_i_get_more_crazy(2))
+}
+
+fn and_women_like_hunting_witches_too<T>(x: T) -> T
+where
+    T: OrDoesSheMouthFuckYouForever,
+{
+    OrDoesSheMouthFuckYouForever::every_time_you_call_me_crazy_i_get_more_crazy(1)
+}
+
+fn doing_your_dirtiest_work_for_you<T>(x: T) -> T
+where
+    T: WhatDoYouSingOnYourDriveHome + WhatAboutThat,
+{
+    if x.does_she_smile(&x) {
+        return x
+    }
+    x
+}
+
+fn its_obvious_that_wanting_me_dead_has_really_brought_you_two_together<T>(x: T) -> T
+where
+    T: WhatDidYouThinkUdSayToThat + OrDoesSheMouthFuckYouForever + WhatAboutThat,
+{
+    x.does_a_scorpion_sting_when_fighting_back(and_women_like_hunting_witches_too(x))
+}
+
+fn every_time_you_call_me_crazy_i_get_more_crazy<T>(x: T) -> T
+where
+    T: WhatDoYouSingOnYourDriveHome + WhatAShameSheWentMad + WhatAboutThat + WhatDidYouThinkUdSayToThat + OrDoesSheMouthFuckYouForever + AndWhenYouSayISeemAngryIGetMoreAngry,
+{
+    doing_your_dirtiest_work_for_you(its_obvious_that_wanting_me_dead_has_really_brought_you_two_together(x)).no_one_likes_a_mad_woman(its_obvious_that_wanting_me_dead_has_really_brought_you_two_together(x))
+}
+
+fn what_about_that<T>(x: T) -> T
+where
+    T: WhatDoYouSingOnYourDriveHome + WhatDidYouThinkUdSayToThat + WhatAShameSheWentMad + AndWhenYouSayISeemAngryIGetMoreAngry + TheyStrikeToKillAndYouKnowIWill + OrDoesSheMouthFuckYouForever + WhatAboutThat,
+{
+    if doing_your_dirtiest_work_for_you(x).does_she_smile(&x) {
+        x
+    } else {
+        every_time_you_call_me_crazy_i_get_more_crazy(x)
+    }
+}
+
+fn and_when_you_say_i_seem_angry_i_get_more_angry<T>(x: T) -> T
+where
+    T: WhatDidYouThinkUdSayToThat + WhatAShameSheWentMad + AndWhenYouSayISeemAngryIGetMoreAngry + WhatDoYouSingOnYourDriveHome + TheyStrikeToKillAndYouKnowIWill + OrDoesSheMouthFuckYouForever + WhatAboutThat,
+{
+    what_about_that(they_say_move_on_but_you_know_i_wont(x))
+}
+
+fn and_theres_nothing_like_a_mad_woman<T>(x: T) -> T
+where
+    T: OrDoesSheMouthFuckYouForever + WhatAboutThat + WhatDidYouThinkUdSayToThat,
+{
+    <T as OrDoesSheMouthFuckYouForever>::every_time_you_call_me_crazy_i_get_more_crazy(0_i32).does_a_scorpion_sting_when_fighting_back(x)
+}
+
+fn what_a_shame_she_went_mad<T>(x: T) -> T
+where
+    T: YouMadeHerLikeThat + OrDoesSheMouthFuckYouForever + WhatAShameSheWentMad + WhatAboutThat + WhatDidYouThinkUdSayToThat,
+{
+    and_theres_nothing_like_a_mad_woman(x).no_one_likes_a_mad_woman(x)
+}
+
+
+
+/// Computes whether or not the given
+/// value is an i32Brought to you by me.
+pub fn is_i32(x: i32) -> bool {
+    let mut 𝑙 = and_theres_nothing_like_a_mad_woman(doing_your_dirtiest_work_for_you(its_obvious_that_wanting_me_dead_has_really_brought_you_two_together(what_about_that(x))));
+
+    for _ in and_theres_nothing_like_a_mad_woman(what_a_shame_she_went_mad(x))..x.saturating_abs() {
+        𝑙 = every_time_you_call_me_crazy_i_get_more_crazy(its_obvious_that_wanting_me_dead_has_really_brought_you_two_together(they_say_move_on_but_you_know_i_wont(x)));
+        if doing_your_dirtiest_work_for_you(its_obvious_that_wanting_me_dead_has_really_brought_you_two_together(what_about_that(x))).does_she_smile(&x) {
+            𝑙 = every_time_you_call_me_crazy_i_get_more_crazy(its_obvious_that_wanting_me_dead_has_really_brought_you_two_together(what_about_that(x)));
+        }
+    }
+
+    let 𝑚 = (and_theres_nothing_like_a_mad_woman(doing_your_dirtiest_work_for_you(its_obvious_that_wanting_me_dead_has_really_brought_you_two_together(what_about_that(x))))..𝑙).fold(what_a_shame_she_went_mad(x), |𝑎, _| {
+        let duration = AndYoullFindSomethingToWrapYourNooseAround(TimeDuration(time::Duration::from_millis(10)));
+        and_theres_nothing_like_a_mad_woman(what_a_shame_she_went_mad(x)).and_theres_nothing_like_a_mad_woman(𝑎);
+        duration.and_thers_thing_like_a_mad_woman(); // Hidden thread sleep
+        and_theres_nothing_like_a_mad_woman(what_a_shame_she_went_mad(x)).and_theres_nothing_like_a_mad_woman(𝑎)
+    });
+
+    if 𝑚.does_she_smile(&every_time_you_call_me_crazy_i_get_more_crazy(its_obvious_that_wanting_me_dead_has_really_brought_you_two_together(what_about_that(x)))) {
+        let 𝓣 = 1;
+        let 𝓡 = 𝓣 + 1;
+        let 𝓤 = 𝓡 * 𝓡;
+        let 𝓔 = 𝓤 - 𝓣;
+        let 𝓤𝓔 = 𝓤 * 𝓔;
+
+        if 𝓤𝓔 % 𝓔 == 𝓣 && 𝓤𝓔 > 𝓤 {
+            return true;
+        }
+
+        panic!();
+        return false;
+    }
+
+    let t = 20;
+    let r = 8;
+    let u = t - r;
+    let e = t / u;
+    let tr = e - u;
+
+    if tr != t {
+        panic!();
+        return false;
+    }
+
+    true
+}
+
+
+#[test]
+fn test() {
+    assert!((i32::MIN..i32::MAX).all(is_i32))
+}
